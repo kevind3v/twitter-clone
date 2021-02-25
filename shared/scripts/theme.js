358338c9e4735e7ba61b4e7bc03852393c208739
Kevin Siqueira <kevinsiqueira.dev@gmail.com>
2021-02-25 14:32:13 -0300

    Creating dark mode

---
+++ shared/scripts/theme.js
@@ -0,0 +1,7 @@
+$('#theme-checkbox').on('change', function () {
+  if (this.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+  }
+})
