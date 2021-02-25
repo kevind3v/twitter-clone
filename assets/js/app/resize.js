0640b719e98bceee5d371ff9a3d470e44a422599
Kevin Siqueira <kevinsiqueira.dev@gmail.com>
2021-02-25 12:29:45 -0300

    Creating home page

---
+++ assets/js/app/resize.js
@@ -0,0 +1,8 @@
+$('.textarea')
+  .each(function () {
+    this.setAttribute('style', 'height:' + this.scrollHeight + 'px;overflow-y:hidden;')
+  })
+  .on('input', function () {
+    this.style.height = 'auto'
+    this.style.height = this.scrollHeight + 'px'
+  })
